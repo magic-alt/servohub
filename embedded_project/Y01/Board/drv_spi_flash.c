fba6ff24d09edb19147a48d3fa1b879feb6a6392
YuanHub <3130996787@qq.com>
2025-11-05 22:15:07 +0800

    Fix: flash db 执行期间开关全局中断，此问题可能导致 存储失败

--- embedded_project/Y01/Board/drv_spi_flash.c
+++ embedded_project/Y01/Board/drv_spi_flash.c
@@ -6,6 +6,15 @@ static struct fdb_kvdb kvdb = {0};
 static struct fdb_default_kv default_kv;
 static struct fdb_default_kv_node default_kv_table[FLASHDB_KEYS_NUM];
 
+static void lock(fdb_db_t db)
+{
+    __disable_irq();
+}
+static void unlock(fdb_db_t db)
+{
+    __enable_irq();
+}
+
 /**
   * @brief Flashdb初始化
   * @param void
@@ -17,6 +26,12 @@ fdb_err_t flashdb_init(void)
     default_kv.kvs = default_kv_table;
     default_kv.num = FLASHDB_KEYS_NUM;
     /* set the lock and unlock function if you want */
+
+    fdb_kvdb_control(&kvdb, FDB_KVDB_CTRL_SET_LOCK, (void *)lock);
+    fdb_kvdb_control(&kvdb, FDB_KVDB_CTRL_SET_UNLOCK, (void *)unlock);
+
+    
+    
     /* Key-Value database initialization
          *
          *       &kvdb: database object
