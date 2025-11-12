fe1e87d8ecc193cb4c1a1f7473fb1993e0162bd4
YuanHub <3130996787@qq.com>
2025-11-12 17:25:47 +0800

    Feat:更新嵌入式工程版本号

--- embedded_project/Y01/Board/version.h
+++ embedded_project/Y01/Board/version.h
@@ -15,8 +15,8 @@ extern "C"
 
 // 版本定义
 #define MAJOR_VERSION           (0x02)              // 主版本号
-#define MINOR_VERSION           (V_ECAT_CIA402)      // 次版本号
-#define REVISION_VERSION        (0x01)              // 修订号
+#define MINOR_VERSION           (V_ECAT_CIA402)     // 次版本号
+#define REVISION_VERSION        (0x02)              // 修订号
 #define BUILD_VERSION           (0x02)              // 构建号
 #define VERSION_ENCODE (((MAJOR_VERSION) << 24) | ((MINOR_VERSION) << 16) | ((REVISION_VERSION) << 8) | (BUILD_VERSION))
 
