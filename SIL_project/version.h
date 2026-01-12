bcd74b23f13ba417e27e528d20c603bd24c5a6a3
guorong.tan <gr_tony@163.com>
2026-01-12 16:39:04 +0800

    Feat：修改虚拟工程的version定义

--- SIL_project/version.h
+++ SIL_project/version.h
@@ -7,44 +7,16 @@ extern "C"
 #endif
 
 // 次版本号定义
-#define V_CAN_CIA402            (0x00)  // 0：CAN CiA402 协议版本
-#define V_CAN_ENCOS             (0x01)  // 1：CAN ENCOS 协议版本
-#define V_CANOPEN_CIA402        (0x02)  // 2：CANopen CiA402 协议版本
-#define V_ECAT_CIA402           (0x03)  // 3：EtherCAT CiA402 协议版本
-#define V_CANFD_CUSTOM          (0x04)  // 4：CANFD 自定义协议版本
+#define V_SIL                   (0xFF)  // 0xFF：SIL 协议版本
 
 // 版本定义
-#define MAJOR_VERSION           (0x01)              // 主版本号
-#define MINOR_VERSION           (V_CAN_CIA402)      // 次版本号
-#define REVISION_VERSION        (0x00)              // 修订号
-#define BUILD_VERSION           (0x00)              // 构建号
+#define MAJOR_VERSION           (0x02)              // 主版本号
+#ifndef MINOR_VERSION           // 若工程编译选项或其他位置未定义次版本号，在此定义
+#define MINOR_VERSION           (V_SIL)      // 次版本号
+#endif // MINOR_VERSION
+#define REVISION_VERSION        (0x02)              // 修订号
+#define BUILD_VERSION           (0x02)              // 构建号
 #define VERSION_ENCODE (((MAJOR_VERSION) << 24) | ((MINOR_VERSION) << 16) | ((REVISION_VERSION) << 8) | (BUILD_VERSION))
-
-#if MINOR_VERSION == V_CAN_CIA402
-#define USE_CAN
-#define USE_CIA402
-#endif
-
-#if MINOR_VERSION == V_CAN_ENCOS
-#define USE_CAN
-#define USE_ENCOS
-#endif
-
-#if MINOR_VERSION == V_CANOPEN_CIA402
-#define USE_CAN
-#define USE_CANOPEN
-#define USE_CIA402
-#endif
-
-#if MINOR_VERSION == V_ECAT_CIA402
-#define USE_ECAT
-#define USE_CIA402
-#endif
-
-#if MINOR_VERSION == V_CANFD_CUSTOM
-#define USE_CAN
-#define USE_CUSTOM
-#endif
 
 
 #ifdef __cplusplus
