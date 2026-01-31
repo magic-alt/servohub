756ae2d3ad106d7ded904cdade548ce59d802fb1
YuanHub <3130996787@qq.com>
2026-01-31 11:12:22 +0800

    Feat:变更版本号 3.0.0.0

--- embedded_project/Y01/Board/version.h
+++ embedded_project/Y01/Board/version.h
@@ -14,12 +14,12 @@ extern "C"
 #define V_CANFD_CUSTOM          (0x04)  // 4：CANFD 自定义协议版本
 
 // 版本定义
-#define MAJOR_VERSION           (0x02)              // 主版本号
+#define MAJOR_VERSION           (0x03)              // 主版本号
 #ifndef MINOR_VERSION           // 若工程编译选项或其他位置未定义次版本号，在此定义
 #define MINOR_VERSION           (V_CAN_CIA402)      // 次版本号
 #endif // MINOR_VERSION
-#define REVISION_VERSION        (0x02)              // 修订号
-#define BUILD_VERSION           (0x02)              // 构建号
+#define REVISION_VERSION        (0x00)              // 修订号
+#define BUILD_VERSION           (0x00)              // 构建号
 #define VERSION_ENCODE (((MAJOR_VERSION) << 24) | ((MINOR_VERSION) << 16) | ((REVISION_VERSION) << 8) | (BUILD_VERSION))
 
 #if MINOR_VERSION == V_CAN_CIA402
